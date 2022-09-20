73013d1eaf1e4175185dee3c48876e4b986bd1d6
krisko15k-web <81244898+krisko15k-web@users.noreply.github.com>
2022-09-20 20:59:09 +0300

    Update ChessValidator.js

--- ChessValidator.js
+++ ChessValidator.js
@@ -1,12 +1,12 @@
 var move = prompt("Choose a move:");
 
-if(move == "a2-a3" || "a2-a4" || "b2-b3" || "b2-b4" || "c2-c3" || 
-"c2-c4" || "d2-d3" || "d2-d4" || "e2-e3" || "e2-e4" || "f2-f3" || 
-"f2-f4" || "g2-g3" || "g2-g4" || "h2-h3" || "h2-h4" || "b1-a3" || 
-"b1-c3" || "g1-f3" || "g1-h3" || "a7-a6" || "a7-a5" || "b7-b6" || 
-"b7-b5" || "c7-c6" || "c7-c5" || "d7-d6" || "d7-d5" || "e7-e6" || 
-"e7-e5" || "f7-f6" || "f7-f5" || "g7-g6" || "g7-g5" || "h7-h6" || 
-"h7-h5" || "b8-a6" || "b8-c6" || "g8-f6" || "g8-h6")
+if(move == "a2-a3" || move == "a2-a4" || move == "b2-b3" || move == "b2-b4" || move == "c2-c3" || move == "c2-c4" 
+|| move == "d2-d3" || move == "d2-d4" || move == "e2-e3" || move == "e2-e4" || move == "f2-f3" || move == "f2-f4" 
+|| move == "g2-g3" || move == "g2-g4" || move == "h2-h3" || move == "h2-h4" || move == "b1-a3" || move == "b1-c3" 
+|| move == "g1-f3" || move == "g1-h3" || move == "a7-a6" || move == "a7-a5" || move == "b7-b6" || move == "b7-b5" 
+|| move == "c7-c6" || move == "c7-c5" || move == "d7-d6" || move == "d7-d5" || move == "e7-e6" || move == "e7-e5" 
+|| move == "f7-f6" || move == "f7-f5" || move == "g7-g6" || move == "g7-g5" || move == "h7-h6" || move == "h7-h5" 
+|| move == "b8-a6" || move == "b8-c6" || move == "g8-f6" || move == "g8-h6")
 {
     alert("All moves are valid");
 }
